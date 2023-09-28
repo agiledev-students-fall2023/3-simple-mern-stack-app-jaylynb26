ec7522ec09acb6e854d97aa1f40e2b56fa064e0e
jaylynb26 <jab9633@nyu.edu>
2023-09-28 00:32:58 -0400

    Paragraphs and photo added.

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import './AboutUs.css'
+import me from './me.jpg'
 
 /**
  * A React component that represents the About Us page of the app.
@@ -9,8 +10,23 @@ import './AboutUs.css'
  const AboutUs = props => {
     return (
       <>
-        <h1>Hi and welcome!</h1>
-        <p>My name is Jaylyn Bido</p>
+        <h1>Hi, and welcome!</h1>
+        <br></br>
+        <p>
+            My name is Jaylyn Bido and I am currently a senior at New York University. I am majoring in Computer Science,
+            and I have three minors in Art History, Psychology, and Web Prorgramming and Applications. I'd love to call
+            New York City home after graduation, but I'm originally from Tampa, Florida and I was raised by Puerto Rican and 
+            Dominican parents. I have three sisters and a dog named Mico!
+        </p>
+        <br></br>
+        <p>
+            A really big hobby of mine is photography. I'm taking a class through NYU Tandon called "Contemporary Techniques
+            in Digital Photography and Imaging" which explores the history and techniques of shooting and editing photos. 
+            Photography has been a creative medium I've enjoyed since I was a little girl, and taking a class that goes into the 
+            minute details of it has been extremely fulfilling! 
+        </p>
+        <br></br>
+        <img src={me} alt='photo of jaylyn bido' width="276px" height="357px"/>
         <p>
           Check out the <Link to="/messages">messages page</Link>.
         </p>
